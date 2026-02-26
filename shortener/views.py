50206227c4d422eca1c3fe48416b1ce7890cc1e8
Manorath Bhatt <bhattmani10@gmail.com>
2026-02-26 22:48:25 +0545

    feat:add shortener form and URL submission logic

--- shortener/views.py
+++ shortener/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from .models import ShortenedURL
+from .forms import ShortenerForm
 def index(request):
-    # This will eventually show our list of URLs
-    urls = ShortenedURL.objects.all()
-    return render(request, 'shortener/index.html', {'urls': urls})
+    if request.method == "POST":
+        form = ShortenerForm(request.POST)
+        if form.is_valid():
+            # Save the new URL to the database
+            form.save()
+            return redirect('index')
+    else:
+        form = ShortenerForm()
+    
+    urls = ShortenedURL.objects.all().order_by('-created_at')
+    return render(request, 'shortener/index.html', {'urls': urls, 'form': form})
