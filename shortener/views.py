ecc8bd717670e75fbe8a507fa7a32f24d6acb4eb
Manorath Bhatt <bhattmani10@gmail.com>
2026-02-26 22:40:37 +0545

    feat: create index view, template and url routing

--- shortener/views.py
+++ shortener/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import ShortenedURL
+def index(request):
+    # This will eventually show our list of URLs
+    urls = ShortenedURL.objects.all()
+    return render(request, 'shortener/index.html', {'urls': urls})
