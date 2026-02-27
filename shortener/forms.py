8d30d5a3b3374374a8e31f19f11090fd5e88301d
Manorath Bhatt <bhattmani10@gmail.com>
2026-02-27 14:37:51 +0545

    feat: add bootstrap styling

--- shortener/forms.py
+++ shortener/forms.py
@@ -2,6 +2,17 @@ from django import forms
 from .models import ShortenedURL
 
 class ShortenerForm(forms.ModelForm):
+    # Overriding fields to add Bootstrap styling (CSS Classes)
+    original_url = forms.URLField(widget=forms.URLInput(attrs={
+        'class': 'form-control form-control-lg',
+        'placeholder': 'Paste your long URL here...'
+    }))
+    
+    short_code = forms.CharField(required=False, widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Custom code (optional)'
+    }))
+
     class Meta:
         model = ShortenedURL
-        fields = ['original_url']
+        fields = ['original_url', 'short_code']
